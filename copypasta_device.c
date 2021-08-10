a516ae5f680cfe13c23c60d48243e8257c5a0021
theloni-monk <theo.acooper@gmail.com>
2021-08-10 20:44:37 +0000

    i hate it here

--- copypasta_device.c
+++ copypasta_device.c
@@ -7,6 +7,7 @@
 #include <linux/cdev.h>
 
 /* For socket etc */
+#include <linux/socket.h>
 #include <linux/net.h>
 #include <net/sock.h>
 #include <linux/tcp.h>
@@ -23,6 +24,7 @@ MODULE_DESCRIPTION("A simple Hello world LKM!");
 MODULE_VERSION("0.1");
 
 #define DEVICE_NAME "copypasta" /* Dev name as it appears in /proc/devices   */
+//mknod copypasta c 42 2
 #define RCP_MAJOR 42			// arbitrary in this case
 #define RCP_MAX_MINORS 5
 
@@ -84,64 +86,68 @@ static void __exit cleanup_mod(void){
 	unregister_chrdev_region(MKDEV(RCP_MAJOR, 0), RCP_MAX_MINORS);
 	printk(KERN_INFO "Goodbye cruel world.\n");
 }
-char __reddit_site_ip[4] = {151, 101, 65, 140}
+char __reddit_site_ip[4] = {151, 101, 65, 140};
 #define REDDIT_ADDR __reddit_site_ip
-char __reddit_api_ip[4] = {172, 67, 174, 211}
+char __reddit_api_ip[4] = {172, 67, 174, 211};
 #define API_ADDR __reddit_api_ip
 #define PORT 80
 /**
  * Allocate and return string buffer recieved from an emulated http request
 */
-#define REQ_LEN = 2048;
-char * kHttpReq(char *addr, char *hostname, char *webargs){
-	char *request = (*char)kalloc(REQ_LEN * sizeof(char)); 
+#define REQ_LEN 2048
+char * kHttpReq(char *addr, char *hostname){
+	printk(KERN_INFO, "request called");
+	char *request = kmalloc(REQ_LEN,0); 
 	sprintf(request,
-			"GET %s HTTP/1.1 \nHost: % s \nConnection: close ",
-			  hostname,
-			  utsname()->nodename);
-
+			"GET %s HTTP/1.1 \nConnection: close ",
+			  hostname);
+	printk(KERN_INFO,"allocated request");
 	/* Build the socket. */
 	struct socket *sock;
 	int err;
 	err = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
-	if (err < 0)
-		return "SOCKET FAILURE";
-
+	if (err < 0){
+		printk(KERN_INFO, "failed to create socket"); 
+		return "err";
+	}
 	/* Build the address. */
 	struct sockaddr_in servaddr;
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr =
-		htonl(create_address(addr));
-
+		htonl(addr);
+	printk(KERN_INFO, "constructed addr");
 	/* Actually connect. */
-	err = control->ops->connect(sock,
+	err = sock->ops->connect(sock,
 								(struct sockaddr *)&servaddr,
 								sizeof(servaddr), O_RDWR);
-
+	if (err< 0){
+		printk(KERN_INFO, "socket failed to connect");
+		return "err";
+	}
+	printk(KERN_INFO, "socket connected");
 	/* Send HTTP request. */
-	struct msghdr msg;
+	struct msghdr * msg;
 	struct iovec iov;
-	msg.msg_name = 0;
-	msg.msg_namelen = 0;
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-	msg.msg_control = NULL;
-	msg.msg_controllen = 0;
-
-	msg.msg_iov->iov_len = REQ_LEN * sizeof(char);
-	msg.msg_iov->iov_base = request;
-
-	int len = sock_sendmsg(sock, &msg);
+	msg->msg_name = 0;
+	msg->msg_namelen = 0;
+	iov.iov_base = request;
+	iov.iov_len = REQ_LEN;
+	iov_iter_init(&msg->msg_iter, READ, &iov, 1, REQ_LEN);
+	msg->msg_control = NULL;
+	msg->msg_controllen = 0;
+	printk(KERN_INFO, "msg constructed");
+	int len = sock_sendmsg(sock, msg);
 	//TODO: handle err
-
+	printk(KERN_INFO, "msg set");
 	/* Read the response. */
-	len = sock_recvmsg(sock, &msg,
-                max_size, 0);
-	char * ret = (*char)kalloc(msg.msg_iov->iov_len * sizeof(char));
-    memcpy(ret, msg.msg_iov->iov_base)
-	free(msg);
+	len = sock_recvmsg(sock, msg, 0);
+	printk(KERN_INFO, "response recieved");
+	//FIXME: might have to do this in multiple recvs
+	char * ret = kmalloc(msg->msg_iter.iov->iov_len,0);
+    memcpy(ret, msg->msg_iter.iov->iov_base, len);
+	printk(KERN_INFO, "copied response to ret buffer");
 	return ret;
 	//FIXME: test this
 }
@@ -150,15 +156,13 @@ char * kHttpReq(char *addr, char *hostname, char *webargs){
  * use api.pushshift.io/reddit/search/submission endpoint to find latest top copypasta addr
  * then fetch the post itself 
 */
-char *getPost(){
-}
+//char * getPost(void );
 
 /**  WRITEME
  * Parse fetched copypasta post into body text and allocate it to the reddit_device field
  * Note: remember to deallocate shit
 */
-void fetchPasta(&reddit_device pdata){
-}
+//void fetchPasta(reddit_device * pdata);
 
 /* 
  * Fetch and parse Copypasta when a process tries to open the device file
@@ -177,10 +181,14 @@ static int device_open(struct inode *inode, struct file *file){
 	struct reddit_device *pdata =
 		container_of(inode->i_cdev, struct reddit_device, cdev);
 
+	pdata->pasta_buffer = kHttpReq(REDDIT_ADDR, "reddit.com/r/copypasta");
+	pdata->pasta_len = 2048;
+	
 	/* validate access to device */
 	file->private_data = pdata;
 	//WRITEME fetch top copypasta post and load it into device pasta buffer
 
+	
 	return SUCCESS;
 }
 
